a71fff82603fb951f6586e4de86e6a8a78e5cc16
manojbishwakarma <manojbishwakarma@neebal.com>
2025-01-28 01:02:48 -0500

    Install mongoose and Fix MongoDB connection error and establish successful connection to Atlas cluster

--- Assignment 1 - Express API/index.js
+++ Assignment 1 - Express API/index.js
@@ -5,12 +5,15 @@ const recipesRoutes = require("./routes/recipesRoutes");
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb+srv://manojbishwakarma88:<M@noj2123>@recipesdatabase.iogvl.mongodb.net/"; // Replace with your MongoDB URI
+  "mongodb+srv://manojbishwakarma88:manoj123@recipesdatabase.iogvl.mongodb.net/";
+
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1); // Exit the process if connection fails
+  });
 app.use(express.json());
 
 app.use("/recipes", recipesRoutes);
