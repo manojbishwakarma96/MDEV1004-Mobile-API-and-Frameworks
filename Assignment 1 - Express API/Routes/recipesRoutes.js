121352828a646785c16f79892bab1399aad30f3c
manojbishwakarma <manojbishwakarma@neebal.com>
2025-01-27 22:15:50 -0500

    index.js initial setup

--- Assignment 1 - Express API/Routes/recipesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getRecipes } = require("../Controllers/recipeController");
-
-const router = express.Router();
-
-router.get("/", getRecipes);
-
-module.exports = router;
--- Assignment 1 - Express API/index.js
+++ Assignment 1 - Express API/index.js
@@ -1,21 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const recipeRoutes = require("./Routes/recipesRoutes");
-
 const app = express();
+
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("Welcome to the Recipe API");
+});
 
-app.use("/api/recipes", recipeRoutes);
+app.get("/api/recipes", (req, res) => {
+  res.json([{ name: "Pasta" }, { name: "Salad" }]);
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
