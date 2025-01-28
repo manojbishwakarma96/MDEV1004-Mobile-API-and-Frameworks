85dc4e20d7853c45745b19a281211bab22106238
LaxmanRky <laxman.rokaya02@gmail.com>
2025-01-28 18:31:10 -0500

    Create Mongoose Schema and Integrate with Controller

---
+++ Assignment 1 - Express API/models/Recipies.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+  recipeName: String,
+  ingredients: [String],
+  cookingTime: Number,
+  difficulty: String,
+  cuisine: String,
+  description: String,
+  photoLink: String,
+  averageRating: Number,
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
