49097d1795ac1ab8239d776f21169c3bad2758b8
manojbishwakarma <manojbishwakarma@neebal.com>
2025-01-29 22:52:42 -0500

    Added the list of our top 20 recipes in JSON format

--- Assignment 1 - Express API/models/Recipies.js
+++ Assignment 1 - Express API/models/Recipies.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
-  recipeName: { type: String, required: true },
-  ingredients: { type: [String], required: true },
-  cookingTime: { type: Number, required: true },
-  difficulty: { type: String, required: true },
-  cuisine: { type: String, required: true },
-  description: { type: String, required: true },
-  photoLink: { type: String, required: true },
-  averageRating: { type: Number, min: 0, max: 5 }
+  recipeName: String,
+  ingredients: [String],
+  cookingTime: Number,
+  difficulty: String,
+  cuisine: String,
+  description: String,
+  photoLink: String,
+  averageRating: Number,
 });
 
 module.exports = mongoose.model("Recipe", recipeSchema);
--- Assignment 1 - Express API/routes/recipesRoutes.js
+++ Assignment 1 - Express API/routes/recipesRoutes.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const { getRecipes } = require("../controllers/recipesController");
 const router = express.Router();
-router.get("/", getRecipes);
+
+// POST a new recipe
+router.post("/", async (req, res) => {
+  try {
+    const newRecipe = new Recipe(req.body);
+    await newRecipe.save();
+    res.status(201).json(newRecipe);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// GET all recipes
+
+router.get("/", async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;
