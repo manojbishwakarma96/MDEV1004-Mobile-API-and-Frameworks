818db0f9b4215f4656b4b51965008cb777df35e5
LaxmanRky <laxman.rokaya02@gmail.com>
2025-01-29 21:53:25 -0500

    Update controller to fetch recipes from MongoDB

--- Assignment 1 - Express API/controllers/recipesController.js
+++ Assignment 1 - Express API/controllers/recipesController.js
@@ -5,4 +5,13 @@ const recipes = [
 
 exports.getRecipes = (req, res) => {
   res.status(200).json({ success: true, data: recipes });
+}
+
+exports.getRecipes = async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.status(200).json({ success: true, data: recipes });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
